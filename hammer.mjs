af3ce09aec6a26f073022976b57ed8ae60e27e67
minwidth0px <153148032+minwidth0px@users.noreply.github.com>
2024-08-04 03:14:00 -0400

    ++

--- hammer.mjs
+++ hammer.mjs
@@ -8,11 +8,11 @@ export async function clean() {
 // Start
 // ------------------------------------------------------------------
 export async function start_client() {
-  await shell('hammer serve example/client/ --dist target/client')
+  await shell('hammer serve src/client/ --dist target/client')
 }
 
 export async function start_server() {
-  await shell('hammer run example/server/index.ts --dist target/server')
+  await shell('hammer run src/server/index.ts --dist target/server')
 }
 export async function start() {
   await Promise.all([start_client(), start_server()])
