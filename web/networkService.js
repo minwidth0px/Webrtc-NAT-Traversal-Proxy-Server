95695c387e4a4ffbbc202e251274280abb7dc13b
minwidth0px <153148032+minwidth0px@users.noreply.github.com>
2024-08-07 15:35:30 -0400

    try to test hosting on gh

---
+++ web/networkService.js
@@ -0,0 +1,15 @@
+// src/client/networkService.mts
+var networkService_default = {
+  useSmoke: false,
+  smokeClient: null,
+  async fetch(url, options) {
+    if (this.useSmoke && this.smokeClient) {
+      return await this.smokeClient.Http.fetch(url, options);
+    } else {
+      return await fetch(url, options);
+    }
+  }
+};
+export {
+  networkService_default as default
+};
