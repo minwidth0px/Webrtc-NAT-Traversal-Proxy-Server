ea299daf24f55a61df3b1dcb45a64998b59920ac
minwidth0px <153148032+minwidth0px@users.noreply.github.com>
2024-08-07 16:58:20 -0400

    ++

--- hammer.mjs
+++ hammer.mjs
@@ -2,17 +2,17 @@
 // Clean
 // ------------------------------------------------------------------
 export async function clean() {
-  await folder('target').delete()
+  await folder('web').delete()
 }
 // ------------------------------------------------------------------
 // Start
 // ------------------------------------------------------------------
 export async function start_client() {
-  await shell('hammer serve src/client/ --dist target/client')
+  await shell('hammer serve src/client/ --dist web/client')
 }
 
 export async function start_server() {
-  await shell('hammer run src/server/index.ts --dist target/server')
+  await shell('hammer run src/server/index.ts --dist web/server')
 }
 export async function start() {
   await Promise.all([start_client(), start_server()])
